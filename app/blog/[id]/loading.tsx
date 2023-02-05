f7dd6c5e9e5b5dda594c07e7f605a419911bbc9e
dilipchandima <dilipchandima@gmail.com>
2023-02-05 17:45:51 +0530

    loading UI

---
+++ app/blog/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center h-96">
+      <h2 className="text-3xl text-blue-500">Loading ...</h2>\
+    </div>
+  );
+}
