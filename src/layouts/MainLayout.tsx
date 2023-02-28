5891d9f15aa53d67dae86deb8f8fd7ef54a50a6b
dilipchandima <dilipchandima@gmail.com>
2023-02-28 11:47:47 +0530

    layout change

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -27,12 +27,31 @@ export const MainLayout = ({ children }: Props) => {
       }`}
       lang="en"
     >
-      <body className="bg-gray-50 dark:bg-gray-900">
-        <Navbar />
+      <body className="bg-gray-50 dark:bg-gray-800 ">
+        <div className="container mx-auto mainGrid">
+          <div className="bg-gray-800 h-screen">
+            {/* <Navbar /> */}
+            <div className="fixed w-[120px]">sample</div>
+          </div>
+          <div className="bg-gray-900">
+            {children}
+            {/* <h1 className="my-5 text-9xl purple-shadow text-gray-50 font-bold">
+              Dileepa
+              <br />
+              Chandima
+            </h1> */}
+          </div>
+          <div className="bg-gray-800 h-screen">
+            <div className="fixed w-[80px]">
+              <MainFooter />
+            </div>
+          </div>
+        </div>
+        {/* <Navbar />
         <div className=" text-gray-900 dark:text-gray-200 pt-20 min-h-screen flex flex-col">
           <div className="grow lg:container  mx-auto">{children}</div>
           <MainFooter />
-        </div>
+        </div> */}
       </body>
     </html>
   );
