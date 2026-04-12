05e85b050a764cd9643e80bb04f5a1361e4b1435
dilipchandima <dilipchandima@gmail.com>
2026-04-13 11:03:52 +1200

    remove unused

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ component: Home })
 
-function App() {
-  return <main className="page-wrap px-4 pb-8 pt-14"></main>
+function Home() {
+  return (
+    <main className="page-wrap px-4 pb-8 pt-14">
+      <h1 className="text-2xl font-semibold tracking-tight text-zinc-900">
+        Dilip Chandima
+      </h1>
+    </main>
+  )
 }
